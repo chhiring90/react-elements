df25583e3e31ca6c9334c450049aee5a3d92857f
chhiring90 <cgtamang90@gmail.com>
2021-04-23 23:27:50 +0545

    1: logo added

---
+++ src/components/Layout/Navigation.js
@@ -0,0 +1,10 @@
+import { ReactComponent as Logo } from '../../assets/images/brand-logo.svg';
+
+const navigation = props => {
+    return (
+        <nav>
+            <Logo />
+        </nav>)
+}
+
+export default navigation;
